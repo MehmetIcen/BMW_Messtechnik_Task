21445370996a5921f1c8d0758ecdab94921620ca
MehmetIcen <mehmet-icen@outlook.de>
2024-06-17 23:46:44 +0200

    Add files via upload

--- Mehmet_Icen_BMW_Messtechnik_Task.py
+++ Mehmet_Icen_BMW_Messtechnik_Task.py
@@ -1,30 +1,32 @@
 import pandas as pd
 import numpy as np
 
-import os
-#Auflisten aller Dateien im aktuellen Verzeichnis (Optional) zur Überprüfung ob CSV-File im selben Path ist
+#Auflisten aller Dateien im aktuellen Verzeichnis (Optional) zur Überprüfung ob CSV-File im selben Project ist zur Verarbeitung
+
+'''import os
 files = os.listdir('.')
-print(files)
-''''
-# CSV-Datei einlesen, hier die CSV-Datei zum überarbeiten geben 'xxx.csv'
+print(files)'''
+
+# CSV-Datei einlesen, hier die CSV-Datei zum überarbeiten geben 'xxx.csv' oder den Pfad zur CSV-Datei
 df = pd.read_csv('Sample_list.csv', sep=';')
 
 # Leere Zellen und 'X'-Zeichen bereinigen aus der Excel-Datei und durch NaN ersetzen "Not a Number" äquivalent zu Null
 df.replace('X', np.nan, inplace=True)
 
-# Leere Zellen in spezifischen Spalten auffüllen,
-# die Funktion Forward Fill übernimmt den Eintrag des letzten Feldes für die kommende Reihe
-df[['ID', 'First Name', 'Last Name']] = df[['ID', 'First Name', 'Last Name']].ffill()
+# Leere Zellen in spezifischen Spalten auffüllen, die Funktion Forward Fill übernimmt den Eintrag des letzten Feldes für die kommende Reihe
+df[['ID']] = df[['ID']].ffill()
+#Provisorische Lösung, Überarbeitung notwendig - ffill() limitiert auf maximal 1, damit Inhalt der CSV-Datei äquivalent zur xlsx-Datei ist
+df[['Last Name']] = df [['Last Name']].ffill(limit = 1)
 
 # Datum in das deutsche Format dd.mm.yyyy umwandeln
 df['Date of birth'] = pd.to_datetime(df['Date of birth'], errors='coerce', dayfirst=True).dt.strftime('%d.%m.%Y')
 
+#Funktion zur Suche nach spezifischer ID
 def find_people_by_id(df, search_id):
     result = df[df['ID'] == search_id]
     return result
 
-# Suche nach der spezifischen ID
-search_id = '50Bb061cB30B461'
+search_id = '5d2feAfbdCAA6B5'
 
 result_df = find_people_by_id(df, search_id)
 print(result_df)
